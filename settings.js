6dd1f525ccd4e8d2130c656e808f9cf941d1fd45
BOT-MAKES162-crypto <Exsanaff@gmail.com>
2025-02-23 03:46:54 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID === undefined ? 'PRABATH-MD~SE1gXDSb#2xHVRpF72fxA3sCB9IKNqsC25JCJS2x5vz5MrBA01KM' : process.env.SESSION_ID,
+    SESSION_ID: process.env.SESSION_ID === undefined ? 'PRABATH-MD~gHMQSKgD#8HZZ-occDKc8fPn4WOaMITNbT3dXyIEvPznecmH3tdU' : process.env.SESSION_ID,
     PREFIX: process.env.PREFIX || '.',
     FOOTER: process.env.FOOTER || "*✦𝐃𝐢𝐝𝐮𝐥𝐚 𝐌𝐃 𝐕𝟐✦*",
     ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/za6ytm.jpg",
